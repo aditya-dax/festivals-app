f36b04f2e44db249511eb8a7896a81f0d66881ba
Aditya Sathe <aditya.sathe@data-axle.com>
2022-08-18 13:42:31 +0530

    initial commit

---
+++ src/main/java/com/assignment/festivals/FestivalsApp1Application.java
@@ -0,0 +1,13 @@
+package com.assignment.festivals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalsApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalsApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/festivals/FestivalsApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.festivals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalsApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
